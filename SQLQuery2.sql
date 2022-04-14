f79316b6805f4644030cbee0642129ddf1b09c57
EAgha <41126467+EAghayev@users.noreply.github.com>
2022-04-14 20:33:26 +0400

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,47 @@
+﻿SELECT * FROM Students
+
+ALTER TABLE Students
+ADD Point INT 
+
+UPDATE Students
+SET Point=98
+WHERE Id=9
+
+SELECT * FROM Students
+JOIN Groups ON Students.GroupId=Groups.Id
+
+CREATE TABLE Certificates
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Name NVARCHAR(20) NOT NULL,
+	MinPoint INT,
+	MaxPoint INT
+)
+
+INSERT INTO Certificates
+VALUES
+('Adi',65,84),
+(N'Şərəf',85,94),
+(N'Yüksək şərəf',95,100)
+
+TRUNCATE TABLE Certificates
+
+SELECT * FROM Certificates
+
+SELECT * FROM Students
+LEFT JOIN Certificates ON Students.Point BETWEEN Certificates.MinPoint AND Certificates.MaxPoint
+
+SELECT * FROM Certificates
+
+CREATE TABLE Departments
+ (
+	Id INT PRIMARY KEY IDENTITY,
+	Name NVARCHAR(20) NOT NULL,
+	DepartmentId INT 
+ )
+
+
+ SELECT D.Id,D.Name,MD.Name AS 'MainDep' FROM Departments AS D
+ LEFT JOIN Departments AS MD ON D.DepartmentId = MD.Id
+
+ SELECT D.Id,D.Name ,(SELECT COUNT(Id) FROM Departments AS SB WHERE SB.DepartmentId = D.Id) AS 'SubDepCount' FROM Departments AS D
